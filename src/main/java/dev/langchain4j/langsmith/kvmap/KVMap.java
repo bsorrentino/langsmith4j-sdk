0a3d728b9d1a77819ec1d7c7cbeb692b5a0ee106
bsorrentino <bartolomeo.sorrentino@gmail.com>
2025-08-17 23:21:50 +0200

    build: update to last openapi specs including only runs api

--- src/main/java/dev/langchain4j/langsmith/kvmap/KVMap.java
+++
@@ -1,6 +0,0 @@
-package dev.langchain4j.langsmith.kvmap;
-
-public interface KVMap {
-
-    java.util.Map<String,Object> getData();
-}
--- src/main/java/dev/langchain4j/langsmith/kvmap/OutputsDataTypeAdapter.java
+++
@@ -1,7 +0,0 @@
-package dev.langchain4j.langsmith.kvmap;
-
-import dev.langchain4j.langsmith.gen.model.Outputs;
-
-public class OutputsDataTypeAdapter extends KVMapDataTypeAdapter<Outputs>  {
-
-}
